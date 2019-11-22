f43dca08b436cf70a4ea7abf6628be47a5bc5d5d
Henry <henryschooltest@gmail.com>
2019-11-22 16:34:07 -0600

    Menu basically works

--- untitled golf game/Assets/Scripts/ChangeScene.cs
+++ untitled golf game/Assets/Scripts/ChangeScene.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public void ChangeToScene (string SceneToChangeTo) {
-        Application.LoadLevel(SceneToChangeTo);
+    public void ChangeToScene (string SceneToChangeTo)
+    {
+        //Application.LoadLevel(SceneToChangeTo);
+        SceneManager.LoadScene(SceneToChangeTo);
 	}
 }
---
+++ untitled golf game/Assets/Scripts/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+    public void playGame () {
+        SceneManager.LoadScene ("SampleScene");
+    }
+
+    public void options () {
+
+    }
+
+    public void exitGame () {
+        Application.Quit ();
+    }
+}
