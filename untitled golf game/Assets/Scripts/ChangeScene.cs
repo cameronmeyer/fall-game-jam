fd443578401a45421f11045b53664d778006398a
Henry <henryschooltest@gmail.com>
2019-11-22 15:18:32 -0600

    Added Some Menu Stuff

---
+++ untitled golf game/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void ChangeToScene (string SceneToChangeTo) {
+        Application.LoadLevel(SceneToChangeTo);
+	}
+}
