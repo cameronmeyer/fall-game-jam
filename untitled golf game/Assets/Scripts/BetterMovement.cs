3e87024804196be0ff25838695a051676cce170b
NoahSims <56094247+NoahSims@users.noreply.github.com>
2019-12-03 12:34:27 -0600

    Began working on a new movement script, not functional yet

---
+++ untitled golf game/Assets/Scripts/BetterMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterMovement : MonoBehaviour
+{
+    public GameObject RearLWheel;
+    public GameObject RearRWheel;
+
+    Vector3 forward = new Vector3(100, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RearLWheel.transform.Rotate(forward * Time.deltaTime);
+        RearRWheel.transform.Rotate(forward * Time.deltaTime);
+    }
+}
